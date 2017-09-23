3d66662fa0652c8f936516bb8101678399037daf
Tmilly <32075722+Tmilly@users.noreply.github.com>
2017-09-23 18:57:52 -0400

    MenuUpdate-2
    Gave Actions to some Menu Items - save/exit
    extended inspector/object builder panels to scale with resolution

--- UMLEditor/src/UMLEditor/Main.java
+++ UMLEditor/src/UMLEditor/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.application.HostServices;
 
 public class Main extends Application {
 
@@ -19,19 +20,22 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(final Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("UMLEditor.fxml"));
-        
         //variable will update based on current file name, new file will be called untitled
-        String fileName = new String ("untitled");
+        String fileName = new String ("Untitled");
         
         window = primaryStage;
         window.setTitle("Team Rocket UML Editor: " + fileName);
 
         Scene scene = new Scene(root, 300, 275);
-        window.getIcons().add(new Image("file:images/Team_Rocket_Logo.jpg"));
+        window.getIcons().add(new Image("https://github.com/tommy-russoniello/juml/tree/master/UMLEditor/images/Team_Rocket_Logo.jpg"));
         window.setScene(scene);
         window.show();
+
+        //gives
+        Controller.getPrimaryStage(window);
     }
+
 
 }

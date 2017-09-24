4a0da8dc79bdf2b53d96bc409cbef18ada92ff3f
Tommy Russoniello <tommy.russoniello@gmail.com>
2017-09-24 14:18:00 -0400

    Change points collection to be stored as deque Add select mode button

--- UMLEditor/src/UMLEditor/Main.java
+++ UMLEditor/src/UMLEditor/Main.java
@@ -34,7 +34,7 @@ public class Main extends Application {
         window.show();
 
         //gives
-        Controller.getPrimaryStage(window);
+        Controller.setPrimaryStage(window);
     }
 
 

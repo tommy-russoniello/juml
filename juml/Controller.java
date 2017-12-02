a9209e32d65a24a88da139b4d252c9692dac0a1c
scarroll142 <scarroll142@gmail.com>
2017-12-02 17:07:12 -0500

    Auto Scroll
    -Pane now scrolls automagically as you drag a node offscreen
    
    -Also added some needed imports

--- juml/Controller.java
+++ juml/Controller.java
@@ -1,5 +1,6 @@
 package juml;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,11 +17,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Map;
 import java.util.Stack;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
@@ -31,6 +35,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.FileChooser;
@@ -112,6 +117,56 @@ public class Controller {
 	public void setPrimaryStage(Stage primaryStage) {
 		window = primaryStage;
 		scene = window.getScene();
+
+		scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, e -> {
+			try{
+			UMLObject node = getObject(e.getTarget());
+			if(node instanceof UMLNode){
+				if(node.originX > scrollPane.getViewportBounds().getWidth()){
+					scrollPane.setHvalue(1);
+				}
+				if(node.originY > scrollPane.getViewportBounds().getHeight()){
+					scrollPane.setVvalue(1);
+				}
+			}else if (node instanceof Relationship && e.getTarget() instanceof Circle){
+				Relationship relationship = (Relationship) node;
+				Pivot pivot = getPivot(relationship, (Circle) e.getTarget());
+				if(pivot.originX > scrollPane.getViewportBounds().getWidth()){
+					scrollPane.setHvalue(1);
+				}
+				if(pivot.originY > scrollPane.getViewportBounds().getHeight()){
+					scrollPane.setVvalue(1);
+				}
+			}
+			}catch(NullPointerException npe){
+
+			}
+		});
+
+		scene.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
+			try{
+				UMLObject node = getObject(e.getTarget());
+				if(node instanceof UMLNode){
+					if(node.originX > scrollPane.getViewportBounds().getWidth()){
+						scrollPane.setHvalue(1);
+					}
+					if(node.originY > scrollPane.getViewportBounds().getHeight()){
+						scrollPane.setVvalue(1);
+					}
+				}else if (node instanceof Relationship && e.getTarget() instanceof Circle){
+					Relationship relationship = (Relationship) node;
+					Pivot pivot = getPivot(relationship, (Circle) e.getTarget());
+					if(pivot.originX > scrollPane.getViewportBounds().getWidth()){
+						scrollPane.setHvalue(1);
+					}
+					if(pivot.originY > scrollPane.getViewportBounds().getHeight()){
+						scrollPane.setVvalue(1);
+					}
+				}
+				}catch(NullPointerException npe){
+
+				}
+		});
 
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
       public void handle(KeyEvent event) {

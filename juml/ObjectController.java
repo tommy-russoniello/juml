d5b35d3ef92f6bb9a34ba33fbadc0352b47a2849
scarroll142 <32077399+scarroll142@users.noreply.github.com>
2017-12-07 07:44:07 -0500

    Update ObjectController.java

--- juml/ObjectController.java
+++ juml/ObjectController.java
@@ -3,6 +3,12 @@ package juml;
 
 /**
  * The Class ObjectController. Extended by UMLNodeContoller and UMLConnectorController extend.
+ * @author Samuel Carroll
+ * @author Torrance Graham
+ * @author Quinn Minnich
+ * @author Thomas Russoniello
+ * @version 0.3
+ * @since 0.3
  */
 public class ObjectController {
   
